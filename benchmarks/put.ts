eb836cafaa60620e88ed42b98e43a0c01b5efa95
Sankaku <sankaku_dlt.45631@outlook.jp>
2024-05-29 12:40:17 +0000

    Update benchmark

--- benchmarks/put.ts
+++ benchmarks/put.ts
@@ -12,19 +12,24 @@ const bench = new Bench({time: 100});
 
 bench
   .add('ICRecord', () => {
-    const icr = ICRecord.create(mapOriginal);
-    ICRecord.map(icr, (v, _k) => v + 1);
+    let icr = ICRecord.create(mapOriginal);
+    for (let i = 0; i < 1000; i++) {
+      icr = ICRecord.put(icr, i.toString(), i + 1);
+    }
+    ICRecord.mergeCache(icr);
   })
   .add('Immutable.Map', async () => {
-    const im = Immutable.Map(mapOriginal);
-    im.map((v, _k) => v + 1);
+    let im = Immutable.Map(mapOriginal);
+    for (let i = 0; i < 1000; i++) {
+      im = im.set(i.toString(), i + 1);
+    }
   });
 
 const run = async () => {
-  await bench.warmup(); // make results more reliable, ref: https://github.com/tinylibs/tinybench/pull/50
+  await bench.warmup();
   await bench.run();
 
-  console.log('map.ts');
+  console.log('put.ts');
   console.table(bench.table());
 };
 
