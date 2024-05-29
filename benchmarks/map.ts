76399c71689a2d9ae926e9c0e5db6a6fa0109d3e
Sankaku <sankaku_dlt.45631@outlook.jp>
2024-05-29 12:33:59 +0000

    Add benchmark

--- benchmarks/map.ts
+++ benchmarks/map.ts
@@ -1,41 +1,30 @@
 /* eslint-disable node/no-extraneous-import */
 import {Bench} from 'tinybench';
+import * as Immutable from 'immutable';
+import {ICRecord} from '../src';
+
+const mapOriginal: Record<string, number> = {};
+for (let i = 0; i < 1000; i++) {
+  mapOriginal[i.toString()] = i;
+}
 
 const bench = new Bench({time: 100});
 
 bench
-  .add('faster task', () => {
+  .add('ICRecord', () => {
+    const icr = ICRecord.create(mapOriginal);
+    ICRecord.map(icr, (v, _k) => v + 1);
   })
-  .add('slower task', async () => {
-    await new Promise(r => setTimeout(r, 1)); // we wait 1ms :)
-  })
-  .todo('unimplemented bench');
+  .add('Immutable.Map', async () => {
+    const im = Immutable.Map(mapOriginal);
+    im.map((v, _k) => v + 1);
+  });
 
 const run = async () => {
-
   await bench.warmup(); // make results more reliable, ref: https://github.com/tinylibs/tinybench/pull/50
   await bench.run();
 
   console.table(bench.table());
-
-  // Output:
-
-  // ┌─────────┬───────────────┬──────────┬────────────────────┬───────────┬─────────┐
-  // │ (index) │   Task Name   │ ops/sec  │ Average Time (ns)  │  Margin   │ Samples │
-  // ├─────────┼───────────────┼──────────┼────────────────────┼───────────┼─────────┤
-  // │    0    │ 'faster task' │ '41,621' │ 24025.791819761525 │ '±20.50%' │  4257   │
-  // │    1    │ 'slower task' │  '828'   │ 1207382.7838323202 │ '±7.07%'  │   83    │
-  // └─────────┴───────────────┴──────────┴────────────────────┴───────────┴─────────┘
-
-  console.table(bench.table(task => ({'Task name': task.name})));
-
-  // Output:
-  // ┌─────────┬───────────────────────┐
-  // │ (index) │       Task name       │
-  // ├─────────┼───────────────────────┤
-  // │    0    │ 'unimplemented bench' │
-  // └─────────┴───────────────────────┘
-
 };
 
 run();
